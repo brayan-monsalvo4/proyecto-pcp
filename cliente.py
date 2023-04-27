8f405d50e286972031e53be22de321bd0f6151bd
Brayan Monsalvo <brayanmonsalvo4@gmail.com>
2023-04-27 16:41:52 -0600

    Cliente arreglado: manejo de excepciones y colores a la terminal identificando usuario y servidor agregados

--- cliente.py
+++ cliente.py
@@ -1,34 +1,62 @@
 import socket
 import sys
 
-# Configurar la conexión al servidor
+#nos permite mostrar colores en la terminal
+from termcolor import colored, cprint
+
+#guardamos direccion y puerto del servidor
 host = "localhost"
 port = 5000
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
+def main():
+    #creamos un socket con el protocolo ipv4 y que sea de tipo TCP
+    usuario = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try:
+    #establecemos un timeout de 10 segundos, si no responde el servidor el programa se cerrara automaticamente.
+    usuario.settimeout(10)
 
-    while True:
+    cprint("Uso: introduce cualquier cosa que quieras preguntar. Para salir escribe exit o presiona Ctrl + C", color="light_magenta", attrs=["bold"])
 
-    # Enviar una pregunta al servidor
-        question = input("cual es tu pregunta: \n")
+    try:
+        while True:
+            #mensaje introducido por el usuario
+            mensaje = input(colored("Usuario: ", color="cyan", attrs=["bold"]))
 
-        if question == "exit":
-            s.close()
-            sys.exit(0)
+            #si el mensaje es exit, se lanza una KeyboardException
+            if mensaje == "exit":
+                raise KeyboardInterrupt  
 
-        s.send(question.encode())
+            #se conecta al servidor y le manda el mensaje del usuario
+            usuario.connect((host, port))
+            usuario.send(mensaje.encode())
 
-        # Recibir la respuesta del servidor
-        response = s.recv(131072).decode()
-        print(f"Respuesta del servidor: {response}")
+            #guarda la respuesta y la imprime en la terminal
+            respuesta = usuario.recv(150000).decode()
+            print(colored("ChatGPT:", color="light_green", attrs=["bold"]), f"{respuesta}", "\n")
 
-except KeyboardInterrupt:
-    print("interrupcion de teclado")
-    s.close()
-    
-# Cerrar la conexión
+    #exception en caso de que el servidor no se encuentre activo
+    except ConnectionRefusedError:
+        print(colored("El servidor no se encuentra activo.", color="light_red", attrs=["bold"]))
+        usuario.close()
+        sys.exit(0)
 
+    #exception en caso de que el timeout se agote (10 segundos)
+    except socket.timeout:
+        print(colored("Se ha agotado el tiempo de espera.", color="light_red", attrs=["bold"]))
+        usuario.close()
+        sys.exit(0)
 
+    #except para un error de socket generico
+    except socket.error:
+        print(colored("Ha surgido un error.", color="light_red", attrs=["bold"]))
+        usuario.close()
+        sys.exit(0)
+
+    #exception especial para finalizar el programa con "exit" o Ctrl + C
+    except KeyboardInterrupt:
+        print(colored("\nHasta luego!", color="light_blue", attrs=["bold"]))
+        usuario.close()
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
